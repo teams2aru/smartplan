83afbd5bdd02f1ef709c276a5dd4af17020f8271
teams2aru <teams2aru23@gmail.com>
2023-04-17 14:30:33 +0530

    RM DASHBOARD

---
+++ app/Views/rmdash.php
@@ -0,0 +1,7 @@
+<main style="margin-top: 58px;">
+    <div class="container pt-4">
+        <h1> <?php $session = session();
+                echo $session->get('username'); // Rm dashboard?> </h1>
+        <h1>Welcome Relationship manager</h1>
+    </div>
+</main>
