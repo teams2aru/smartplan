ccbdc70161ed62904486bbab7ce74badc70f4601
teams2aru <team2aru23@gmail.com>
2023-04-18 09:59:11 +0100

    Admin Dashboard edited

--- app/Views/admindash.php
+++ app/Views/admindash.php
@@ -1,7 +1,7 @@
 <main style="margin-top: 58px;">
     <div class="container pt-4">
-        <h1> <?php $session = session();
-                echo $session->get('username'); ?> </h1>
+        <h1> <?php $session = session(); // Admin dashboard
+                echo $session->get('username'); ?> </h1> 
         <h1>Welcome Admin</h1>
     </div>
 </main>
